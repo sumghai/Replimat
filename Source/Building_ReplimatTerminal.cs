4aa218d23eda64ca4743e2949688914ed99ec7a6
Robin Chang <sumdumghai@gmail.com>
2017-08-20 19:41:54 +1200

    Add first version of Replimat Terminal code
    For debugging purposes, the terminal should produce free Fine Meals.
    
    TODO - modify Core assembly to accept Replimat Terminals as food source.

---
+++ Source/Building_ReplimatTerminal.cs
@@ -0,0 +1,48 @@
+﻿using Verse;
+using Verse.Sound;
+using RimWorld;
+
+namespace Replimat
+{
+    public class Building_ReplimatTerminal : Building
+    {
+        public CompPowerTrader powerComp;
+
+        public static int CollectDuration = 50;
+
+        public bool CanDispenseNow
+        {
+            get
+            {
+                return this.powerComp.PowerOn; // TODO - Add condition to check if there is enough replicator feedstock
+            }
+        }
+
+        public virtual ThingDef DispensableDef
+        {
+            get
+            {
+                return ThingDefOf.MealFine;
+            }
+        }
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            this.powerComp = base.GetComp<CompPowerTrader>();
+        }
+
+        public virtual Thing TryDispenseFood()
+        {
+            if (!this.CanDispenseNow)
+            {
+                return null;
+            }
+            
+            this.def.building.soundDispense.PlayOneShot(new TargetInfo(base.Position, base.Map, false));
+            Thing thing2 = ThingMaker.MakeThing(ThingDefOf.MealFine, null);
+            return thing2;
+        }
+    }
+}
+
---
+++ Source/Replimat_Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace Replimat
+{
+    public class Replimat_Main
+    {
+
+        // Patch into Rimworld.Toils_Ingest
+        public static Toil TakeMealFromReplimatTerminal(TargetIndex ind, Pawn eater)
+        {
+            Toil toil = new Toil();
+            toil.initAction = delegate
+            {
+                Pawn actor = toil.actor;
+                Job curJob = actor.jobs.curJob;
+                Building_ReplimatTerminal building_ReplimatTerminal = (Building_ReplimatTerminal)curJob.GetTarget(ind).Thing;
+                Thing thing = building_ReplimatTerminal.TryDispenseFood();
+                if (thing == null)
+                {
+                    actor.jobs.curDriver.EndJobWith(JobCondition.Incompletable);
+                    return;
+                }
+                actor.carryTracker.TryStartCarry(thing);
+                actor.CurJob.SetTarget(ind, actor.carryTracker.CarriedThing);
+            };
+            toil.FailOnCannotTouch(ind, PathEndMode.Touch);
+            toil.defaultCompleteMode = ToilCompleteMode.Delay;
+            toil.defaultDuration = Building_ReplimatTerminal.CollectDuration;
+            return toil;
+        }
+
+
+
+    }
+}
