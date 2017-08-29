fb7471d5623069a4b0a68c4f321164fba0774438
Robin Chang <sumdumghai@gmail.com>
2017-08-29 21:23:05 +1200

    Add Replimat inspector tab to terminals
    For #1.
    
    Tabs are currently empty.

---
+++ Source/ITab_Replimat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace Replimat
+{
+    public class ITab_Replimat : ITab
+    {
+        private static readonly Vector2 WinSize = new Vector2(300f, 480f);
+
+        public ITab_Replimat()
+        {
+            this.labelKey = "TabReplimat";
+        }
+
+        protected override void FillTab()
+        {
+            Rect position = new Rect(0f, 0f, ITab_Replimat.WinSize.x, ITab_Replimat.WinSize.y).ContractedBy(10f);
+        }
+    }
+}
