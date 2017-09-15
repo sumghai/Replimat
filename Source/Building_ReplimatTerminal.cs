21a4023df0e35539f72f064577cab15cb0518872
Robin Chang <sumdumghai@gmail.com>
2017-09-15 21:45:28 +1200

    Draw feedstock from tanks when replicating meals
    For #4.
    
    Needs tuning.

--- Source/Building_ReplimatTerminal.cs
+++ Source/Building_ReplimatTerminal.cs
@@ -20,11 +20,62 @@ namespace Replimat
 
         static System.Random rnd = new System.Random();
 
+        public bool hasReplimatTanks;
+
+        public bool ConsumeFeedstock(float feedstockNeeded, bool consumeForReal)
+        {
+            List<Building_ReplimatFeedTank> feedstockTanks = powerComp.PowerNet.powerComps.Where(x => x.parent is Building_ReplimatFeedTank).Select(x => x.parent as Building_ReplimatFeedTank).ToList();
+
+            float totalAvailableFeedstock = 0f;
+
+            if (feedstockTanks.Count() > 0)
+            {
+                hasReplimatTanks = true;
+                
+                feedstockTanks.Shuffle();
+
+                foreach (var currentTank in feedstockTanks)
+                {
+                    totalAvailableFeedstock += currentTank.GetComp<CompFeedstockTank>().StoredFeedstock;
+                }
+
+                Log.Message("Replimat - " + totalAvailableFeedstock.ToString() + " feedstock available across " + feedstockTanks.Count().ToString() + " tanks");
+
+                if (totalAvailableFeedstock >= feedstockNeeded)
+                {
+                    float feedstockLeftToConsume = feedstockNeeded;
+
+                    foreach (var currentTank in feedstockTanks)
+                    {
+                        float num = Math.Min(feedstockLeftToConsume, currentTank.GetComp<CompFeedstockTank>().StoredFeedstock);
+
+                        if (consumeForReal == true)
+                        {
+                            currentTank.GetComp<CompFeedstockTank>().DrawFeedstock(num);
+                        }
+
+                        feedstockLeftToConsume -= num;
+                    }
+
+                    if (feedstockLeftToConsume <= 0f)
+                    {
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                hasReplimatTanks = false;
+            }
+
+            return false;
+        }
+
         public bool CanDispenseNow
         {
             get
             {
-                return this.powerComp.PowerOn; // TODO - Add condition to check if there is enough replicator feedstock
+                return this.powerComp.PowerOn && ConsumeFeedstock(1,false);
             }
         }
 
@@ -65,6 +116,7 @@ namespace Replimat
             }
 
             ReplicatingTicks = GenTicks.SecondsToTicks(2f);
+            ConsumeFeedstock(1, true);
             this.def.building.soundDispense.PlayOneShot(new TargetInfo(base.Position, base.Map, false));
         }
 
