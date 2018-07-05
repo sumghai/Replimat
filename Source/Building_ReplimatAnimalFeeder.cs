deaf4b8de0f109de43f8c3aa5aaefbdb807198c5
Robin Chang <sumdumghai@gmail.com>
2018-07-06 10:04:03 +1200

    Add art and initial framework for Replimat Animal Feeder (WIP)
    For #15.
    
    Basic functionality not yet implemented.

---
+++ Source/Building_ReplimatAnimalFeeder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace Replimat
+{
+    class Building_ReplimatAnimalFeeder : Building_Storage
+    {
+        public CompPowerTrader powerComp;
+
+        
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            this.powerComp = base.GetComp<CompPowerTrader>();
+        }
+
+        public override IEnumerable<IntVec3> AllSlotCells()
+        {
+            yield return Position;
+        }
+    }
+}
