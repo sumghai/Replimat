6a78587b176a244c9a6440d784f32cd86075838c
Robin Chang <sumdumghai@gmail.com>
2021-01-23 19:43:03 +0800

    Create custom corpse filter tab
    For #43

---
+++ Source/ITab_CorpseFilters.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Replimat
+{
+    public class ITab_CorpseFilters : ITab_Storage
+    {
+        public override bool IsPrioritySettingVisible => false;
+
+        public ITab_CorpseFilters()
+        {
+            labelKey = "TabCorpseFilters";
+        }
+    }
+}
