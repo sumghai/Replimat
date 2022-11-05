816965918cdf732368a27a6217b56a35b5452162
sumghai <sumdumghai@gmail.com>
2022-11-05 16:44:09 +1300

    Patch food inspector string to indicate if food is replicated

---
+++ Source/Patches/Harmony_Thing_GetInspectString.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System.Text;
+using Verse;
+
+namespace Replimat.Patches
+{
+    [HarmonyPatch(typeof(ThingWithComps), nameof(ThingWithComps.GetInspectString))]
+    public static class Harmony_Thing_GetInspectString
+    {
+        public static void Postfix(ThingWithComps __instance, ref string __result)
+        {
+            if (__instance.def.IsIngestible && ReplimatUtility.IsReplicatedFood(__instance))
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.Append(__result);
+                stringBuilder.AppendInNewLine("ReplicatedMeal".Translate().CapitalizeFirst());
+                __result = stringBuilder.ToString();
+            }
+        }
+    }
+}
