96b1a66841d418a238911edfd4b0c6ec8fa66ce3
sumghai <sumdumghai@gmail.com>
2022-11-05 14:46:52 +1300

    Patch job report string when feeding babies with replicated baby food
    For #70

---
+++ Source/Patches/Harmony_JobDriver_GetReport.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace Replimat.Patches
+{
+    [HarmonyPatch(typeof(JobDriver), nameof(JobDriver.GetReport))]
+    public static class Harmony_JobDriver_GetReport
+    {
+        public static void Postfix(ref string __result, Pawn ___pawn)
+        {
+            Job job = ___pawn.CurJob;
+            
+            if (job.def.reportString == JobDefOf.BottleFeedBaby.reportString && job.targetB.Thing is Building_ReplimatTerminal)
+            {
+                __result = job.def.reportString.Replace("TargetA", job.targetA.Thing.LabelShort).Replace("TargetB", "ReplicatedFoodPrefix".Translate() + ThingDefOf.BabyFood.label);
+            }
+        }
+    }
+}
