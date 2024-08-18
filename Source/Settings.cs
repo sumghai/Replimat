c17ab787ec45ed5b1561549b265253c844b243a3
sumghai <sumghai@users.noreply.github.com>
2024-08-18 16:34:26 +1200

    Overhaul mod settings UI

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -12,6 +12,7 @@ namespace Replimat
         public override void ExposeData()
         {
             base.ExposeData();
+            var defaults = new Settings();
             Scribe_Values.Look(ref PrioritizeFoodQuality, "PrioritizeFoodQuality", true, true);
             Scribe_Values.Look(ref EnableIncidentSpill, "EnableIncidentSpill", true, true);
             Scribe_Values.Look(ref EnableIncidentKibble, "EnableIncidentKibble", true, true);
@@ -19,28 +20,42 @@ namespace Replimat
 
         public void Draw(Rect canvas)
         {
-            var listingStandard = new Listing_Standard();
+            var listingStandard = new Listing_Standard()
+            {
+                ColumnWidth = (canvas.width - Window.StandardMargin) / 2
+            };
+
             listingStandard.Begin(canvas);
 
-            // Do general settings
-            Text.Font = GameFont.Medium;
-            listingStandard.Label("Replimat_Settings_HeaderGeneral".Translate());
-            Text.Font = GameFont.Small;
+            // First Column - general
             listingStandard.Gap();
-
+            listingStandard.Header("Replimat_Settings_HeaderGeneral".Translate());
             listingStandard.CheckboxLabeled("Replimat_Settings_PrioritizeFoodQuality_Title".Translate(),
                 ref PrioritizeFoodQuality, "Replimat_Settings_PrioritizeFoodQuality_Desc".Translate());
+
+            // Second Column - incidents
+            listingStandard.NewColumn();
             listingStandard.Gap();
 
-            // Do incident settings
-            Text.Font = GameFont.Medium;
-            listingStandard.Label("Replimat_Settings_HeaderIncidents".Translate());
-            Text.Font = GameFont.Small;
-            listingStandard.Gap();
-
+            listingStandard.Header("Replimat_Settings_HeaderIncidents".Translate());
             listingStandard.CheckboxLabeled("Replimat_Settings_EnableIncidentSpill_Title".Translate(), ref EnableIncidentSpill, "Replimat_Settings_EnableIncidentSpill_Desc".Translate());
             listingStandard.CheckboxLabeled("Replimat_Settings_EnableIncidentKibble_Title".Translate(), ref EnableIncidentKibble, "Replimat_Settings_EnableIncidentKibble_Desc".Translate());
+
             listingStandard.End();
+        }
+    }
+
+    public static class UI_Extensions
+    {
+        public static void Header(this Listing_Standard list, TaggedString headerTitle)
+        {
+            Rect rect = list.GetRect(Text.CalcHeight(headerTitle, list.ColumnWidth));
+            Widgets.DrawBoxSolid(rect, Widgets.OptionUnselectedBGFillColor);
+            TextAnchor anchor = Text.Anchor;
+            Text.Anchor = TextAnchor.MiddleCenter;
+            Widgets.Label(rect, headerTitle);
+            Text.Anchor = anchor;
+            list.Gap(4);
         }
     }
 }
